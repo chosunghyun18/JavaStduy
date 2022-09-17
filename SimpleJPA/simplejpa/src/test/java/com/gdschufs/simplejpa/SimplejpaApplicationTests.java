5a9e9ab00f2515a9673c3d9d7067f6cba7b35aec
chosunghyun18 <krismon@naver.com>
2022-09-17 18:34:39 +0900

    [CREATE] jpa ex

---
+++ SimpleJPA/simplejpa/src/test/java/com/gdschufs/simplejpa/SimplejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdschufs.simplejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplejpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
