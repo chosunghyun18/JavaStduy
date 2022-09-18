2adb9d55df99d34243242a8e8cae0c459fc4d416
chosunghyun18 <krismon@naver.com>
2022-09-18 11:57:44 +0900

    [CREATE] jpa simple page 2

--- SimpleJPA/simplejpa2/src/main/java/com/gdschufs/simplejpa/api/MemberApiController.java
+++ SimpleJPA/simplejpa2/src/main/java/com/gdschufs/simplejpa/api/MemberApiController.java
@@ -1,13 +1,13 @@
 package com.gdschufs.simplejpa.api;
 
-import com.gdschufs.simplejpa.dto.MemberDto;
+import static com.gdschufs.simplejpa.dto.MemberDto.*;
 import com.gdschufs.simplejpa.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.gdschufs.simplejpa.dto.MemberDto.*;
+
 
 @RequiredArgsConstructor
 @RestController
