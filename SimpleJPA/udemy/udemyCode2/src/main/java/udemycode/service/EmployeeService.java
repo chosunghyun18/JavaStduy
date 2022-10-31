f5ab0b35845378a0969404459d7f1ed4a8ea9e2a
chosunghyun18 <krismon@naver.com>
2022-10-31 16:30:17 +0900

    [create] jpa data-rest

--- SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeService.java
+++ SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeService.java
@@ -1,15 +1,15 @@
-package udemycode.service;
-
-import udemycode.domain.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    public List<Employee> findAll();
-
-    public Employee findById(int theId);
-
-    public void save(Employee theEmployee);
-
-    public void deleteById(int theId);
-}
+//package udemycode.service;
+//
+//import udemycode.domain.Employee;
+//
+//import java.util.List;
+//
+//public interface EmployeeService {
+//    public List<Employee> findAll();
+//
+//    public Employee findById(int theId);
+//
+//    public void save(Employee theEmployee);
+//
+//    public void deleteById(int theId);
+//}
--- SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeServiceImpl.java
+++ SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeServiceImpl.java
@@ -1,45 +1,45 @@
-package udemycode.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import udemycode.dao.EmployeeRepository;
-import udemycode.domain.Employee;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class EmployeeServiceImpl implements EmployeeService{
-
-    private final EmployeeRepository employeeRepository;
-
-    @Override
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
-    // remove @transactional cause JPARepository provide it
-    @Override
-    public Employee findById(int theId) {
-        // Optional for null check
-        Optional<Employee> result = employeeRepository.findById(theId);
-
-        Employee employee = null;
-
-        if(result.isPresent()){
-            employee = result.get();
-        }else {throw new RuntimeException("Did not find employee id"+theId);}
-
-        return employee;
-    }
-
-    @Override
-    public void save(Employee theEmployee) {
-        employeeRepository.save(theEmployee);
-    }
-
-    @Override
-    public void deleteById(int theId) {
-        employeeRepository.deleteById(theId);
-    }
-}
+//package udemycode.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import udemycode.dao.EmployeeRepository;
+//import udemycode.domain.Employee;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class EmployeeServiceImpl implements EmployeeService{
+//
+//    private final EmployeeRepository employeeRepository;
+//
+//    @Override
+//    public List<Employee> findAll() {
+//        return employeeRepository.findAll();
+//    }
+//    // remove @transactional cause JPARepository provide it
+//    @Override
+//    public Employee findById(int theId) {
+//        // Optional for null check
+//        Optional<Employee> result = employeeRepository.findById(theId);
+//
+//        Employee employee = null;
+//
+//        if(result.isPresent()){
+//            employee = result.get();
+//        }else {throw new RuntimeException("Did not find employee id"+theId);}
+//
+//        return employee;
+//    }
+//
+//    @Override
+//    public void save(Employee theEmployee) {
+//        employeeRepository.save(theEmployee);
+//    }
+//
+//    @Override
+//    public void deleteById(int theId) {
+//        employeeRepository.deleteById(theId);
+//    }
+//}
