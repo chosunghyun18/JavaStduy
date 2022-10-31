aa64f1cd3d45cc8a8dab64a0cbdfde00212751af
chosunghyun18 <krismon@naver.com>
2022-10-31 14:55:06 +0900

    [add] add spring data rest in gradle

--- SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeServiceImpl.java
+++ SimpleJPA/udemy/udemyCode2/src/main/java/udemycode/service/EmployeeServiceImpl.java
@@ -1,11 +1,7 @@
 package udemycode.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import udemycode.dao.EmployeeDAO;
 import udemycode.dao.EmployeeRepository;
 import udemycode.domain.Employee;
 
