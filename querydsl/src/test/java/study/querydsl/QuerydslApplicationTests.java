bb13b7c4d87b4d100b6baf428bf1a861d38e868f
chosunghyun18 <krismon@naver.com>
2023-01-20 00:03:06 +0900

    test: add the test code of the querydsl

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,41 @@
 package study.querydsl;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.domain.helloworld.model.Hello;
+import study.querydsl.domain.helloworld.model.QHello;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void contextHelloLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
 
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+		QHello qHello = new QHello("hi"); // no space in variable
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		assertThat(result).isEqualTo(hello);
+		// test lombok
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
+	}
 }
