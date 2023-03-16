0e59cf696ba203ae693134a195c2d9c355d3059a
chosunghyun18 <krismon@naver.com>
2023-03-16 10:40:32 +0900

    feat : Member Repository interface

---
+++ SimpleJPA/simplejpa5/src/main/java/gnuvil/simplejpa5/cms/domain/Member.java
@@ -0,0 +1,19 @@
+package gnuvil.simplejpa5.cms.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Member {
+    private Long id;
+
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ SimpleJPA/simplejpa5/src/main/java/gnuvil/simplejpa5/object/tickets/Ticket.java
@@ -0,0 +1,4 @@
+package gnuvil.simplejpa5.object.tickets;
+
+public class Ticket {
+}
