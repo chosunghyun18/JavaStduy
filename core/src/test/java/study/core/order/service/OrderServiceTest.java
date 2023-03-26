f62e35041fd5c83b060e41b1f9d4d74a8c138c7c
chosunghyun18 <krismon@naver.com>
2023-03-26 19:23:56 +0900

    test: add create order test

--- core/src/test/java/study/core/order/service/OrderServiceTest.java
+++ core/src/test/java/study/core/order/service/OrderServiceTest.java
@@ -1,2 +1,28 @@
-package study.core.order.service;public class OrderServiceTest {
+package study.core.order.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import study.core.member.model.Grade;
+import study.core.member.model.Member;
+import study.core.member.service.MemberService;
+import study.core.member.service.MemberServiceImpl;
+import study.core.order.model.Order;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.creteOrder(memberId,"ItemA",1000000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
 }
