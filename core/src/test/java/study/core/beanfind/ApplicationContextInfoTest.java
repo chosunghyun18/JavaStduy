104df2b8b26ea7c413b0ae2fd139f7086c22267f
chosunghyun18 <krismon@naver.com>
2023-03-28 20:15:19 +0900

    test : add getBeanDefinitioName

--- core/src/test/java/study/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/study/core/beanfind/ApplicationContextInfoTest.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.core.AppConfig;
 
@@ -31,5 +32,32 @@ public class ApplicationContextInfoTest {
 
         }
     }
+    @Test
+    @DisplayName("show Application Bean")
+    void findAppBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName:beanDefinitionNames){
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // Role ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+
+            // Role ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name="+beanDefinitionName +"object="+bean);
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
 
 }
